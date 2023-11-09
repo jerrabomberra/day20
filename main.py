c458b3056911a0a560d3a4d7a3c547912d343eb8
Doug Gordon <gordononline@bigpond.com>
2023-11-10 07:46:31 +1100

    some instructions

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # snake game 3 squares - listen up left down right arrows
+# possible options are: once move key is detected 
+# get position of each part of the snake and each snake part and move the previous one to that one
+# each time it eats a bit of food, add another bit to the tail
+# if head touches edge then game over (end)
+# if snake touches itself then game over (end)
 
 from turtle import Turtle, Screen 
 
