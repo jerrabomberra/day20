95ed263863738d9471d5c5bece3be0153220af54
Doug Gordon <gordononline@bigpond.com>
2023-11-10 22:24:54 +1100

    not working

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ screen.tracer(0)
 starting_position = [(0,0),(-20,0),(-40,0)]
 
 segments = []
+c_pos =[]
 
 for i in starting_position:
     new_segment = Turtle('square')
@@ -26,10 +27,14 @@ for i in starting_position:
     new_segment.penup()    
     new_segment.goto(i)
     segments.append(new_segment)
-        
+#  for i in segments:   
+# 
 
-
-# print(segments)
+# cp = segments[0]
+# c_pos.append(segments[0].pos())
+# print(c_pos[0])
+# # cp.left(90)
+# # print(segments)
 
 game_is_on = True
 
@@ -38,7 +43,9 @@ while game_is_on:
     time.sleep(1)
     for seg in segments:
         seg.forward(10)
-        
+        c_pos.append(seg.pos())
+        for pos in c_pos:
+            seg.goto(c_pos[pos+1])
     # break
     
 
