a8bddbaa31637243f08d074b13b8e36f8bcc5f5e
Doug Gordon <gordononline@bigpond.com>
2023-11-17 08:13:39 +1100

    linted

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from turtle import Turtle, Screen 
+from turtle import Screen
 from snake import Snake
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('Snake Game')
+screen.bgcolor("black")
+screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
